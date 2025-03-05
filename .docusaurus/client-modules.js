3616ed0064108c29a75ef4f21179b3258d026753
SofiaMari <sofiamari1999@gmail.com>
2025-03-05 11:21:39 +0100

    removed docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/infima/dist/css/default/default.css"),
-  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/sofia/Scrivania/DocusaurusTest/src/css/custom.css"),
-];
