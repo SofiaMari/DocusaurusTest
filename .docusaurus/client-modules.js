7d5688f8d73718ccc2d755e2872bc1f272009b71
SofiaMari <sofiamari1999@gmail.com>
2025-03-05 11:07:06 +0100

    package-lock.json

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/infima/dist/css/default/default.css"),
+  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/sofia/Scrivania/DocusaurusTest/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/sofia/Scrivania/DocusaurusTest/src/css/custom.css"),
+];
